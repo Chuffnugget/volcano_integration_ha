7c0eb0e5a64336bd14affd2d617c433fd3915af0
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-12 19:45:17 +0000

    Create config_flow.py

---
+++ custom_components/volcano_integration_ha/config_flow.py
@@ -0,0 +1,12 @@
+from homeassistant import config_entries
+import voluptuous as vol
+
+class VolcanoIntegrationConfigFlow(config_entries.ConfigFlow, domain="volcano_integration_ha"):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Volcano Integration HA", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({vol.Required("address"): str}),
+        )
