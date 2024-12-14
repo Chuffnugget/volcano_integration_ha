13432751089678392cb0eb05e42969bd0f898114
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-14 01:18:47 +0000

    Update switch.py

--- custom_components/volcano_integration_ha/switch.py
+++ custom_components/volcano_integration_ha/switch.py
@@ -1,18 +1,42 @@
 from homeassistant.components.switch import SwitchEntity
 
-class VolcanoSwitch(SwitchEntity):
-    def __init__(self, coordinator, name, data_key, write_func):
+class GATTFanSwitch(SwitchEntity):
+    """Entity to control the fan."""
+
+    def __init__(self, coordinator, device_info):
         self.coordinator = coordinator
-        self._attr_name = name
-        self.data_key = data_key
-        self.write_func = write_func
+        self._attr_name = "Volcano Fan"
+        self._attr_device_info = device_info
 
     @property
     def is_on(self):
-        return self.coordinator.data[self.data_key]
+        return self.coordinator.data.get("fan")
 
     async def async_turn_on(self):
-        await self.write_func(True)
+        async with BleakClient(self.coordinator.address) as client:
+            await client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
 
     async def async_turn_off(self):
-        await self.write_func(False)
+        async with BleakClient(self.coordinator.address) as client:
+            await client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
+
+
+class GATTHeatSwitch(SwitchEntity):
+    """Entity to control the heat."""
+
+    def __init__(self, coordinator, device_info):
+        self.coordinator = coordinator
+        self._attr_name = "Volcano Heat"
+        self._attr_device_info = device_info
+
+    @property
+    def is_on(self):
+        return self.coordinator.data.get("heat")
+
+    async def async_turn_on(self):
+        async with BleakClient(self.coordinator.address) as client:
+            await client.write_gatt_char("1011000f-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
+
+    async def async_turn_off(self):
+        async with BleakClient(self.coordinator.address) as client:
+            await client.write_gatt_char("1011000f-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
