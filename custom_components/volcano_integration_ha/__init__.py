71e82c26171fb7d28fd591391cb9fd70372941d8
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-14 03:24:24 +0000

    Update __init__.py

--- custom_components/volcano_integration_ha/__init__.py
+++ custom_components/volcano_integration_ha/__init__.py
@@ -1,14 +1,21 @@
 import logging
-import asyncio
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .coordinator import VolcanoCoordinator
+
+DOMAIN = "volcano_integration_ha"
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config_entry):
-    """Set up the Volcano Hybrid integration."""
-    hass.data.setdefault("volcano_integration_ha", {})
-    hass.data["volcano_integration_ha"][config_entry.entry_id] = {
-        "address": config_entry.data["address"]
-    }
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Set up Volcano Integration HA."""
+    address = config_entry.data["address"]
+    coordinator = VolcanoCoordinator(hass, address)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
     for platform in ["sensor", "switch", "number"]:
         hass.async_create_task(
@@ -18,16 +25,14 @@ async def async_setup_entry(hass, config_entry):
     return True
 
 
-async def async_unload_entry(hass, config_entry):
-    """Unload the Volcano Hybrid integration."""
-    unload_results = await asyncio.gather(
-        *[
-            hass.config_entries.async_forward_entry_unload(config_entry, platform)
-            for platform in ["sensor", "switch", "number"]
-        ]
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Unload Volcano Integration HA."""
+    coordinator = hass.data[DOMAIN].pop(config_entry.entry_id)
+    await coordinator.shutdown()
+
+    unload_ok = all(
+        await hass.config_entries.async_forward_entry_unload(config_entry, platform)
+        for platform in ["sensor", "switch", "number"]
     )
 
-    if all(unload_results):
-        hass.data["volcano_integration_ha"].pop(config_entry.entry_id)
-
-    return all(unload_results)
+    return unload_ok
