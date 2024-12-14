c348c6bef29b799d3810edaf35f23b58c9ade76a
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-14 01:18:36 +0000

    Update sensor.py

--- custom_components/volcano_integration_ha/sensor.py
+++ custom_components/volcano_integration_ha/sensor.py
@@ -1,11 +1,39 @@
 from homeassistant.components.sensor import SensorEntity
 
-class VolcanoSensor(SensorEntity):
-    def __init__(self, coordinator, name, data_key):
+class GATTConnectionStatus(SensorEntity):
+    """Entity to track Bluetooth connection status."""
+
+    def __init__(self, coordinator, device_info):
         self.coordinator = coordinator
-        self._attr_name = name
-        self.data_key = data_key
+        self._attr_name = "Volcano Connection Status"
+        self._attr_device_info = device_info
 
     @property
     def native_value(self):
-        return self.coordinator.data[self.data_key]
+        return "Connected" if self.coordinator.connected else "Disconnected"
+
+
+class GATTSerialNumber(SensorEntity):
+    """Entity to show the serial number."""
+
+    def __init__(self, coordinator, device_info):
+        self.coordinator = coordinator
+        self._attr_name = "Volcano Serial Number"
+        self._attr_device_info = device_info
+
+    @property
+    def native_value(self):
+        return self.coordinator.data.get("serial_number")
+
+
+class GATTTemperatureSensor(SensorEntity):
+    """Entity to monitor the current temperature."""
+
+    def __init__(self, coordinator, device_info):
+        self.coordinator = coordinator
+        self._attr_name = "Volcano Current Temperature"
+        self._attr_device_info = device_info
+
+    @property
+    def native_value(self):
+        return self.coordinator.data.get("temperature")
