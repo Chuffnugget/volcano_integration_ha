fdd1392680225b6b575050414918766b45a1ea6a
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-14 02:59:33 +0000

    Update switch.py

--- custom_components/volcano_integration_ha/switch.py
+++ custom_components/volcano_integration_ha/switch.py
@@ -1,4 +1,5 @@
-from . import GATTDeviceCoordinator
+from homeassistant.components.switch import SwitchEntity
+from .coordinator import GATTDeviceCoordinator
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up Volcano Hybrid switch entities."""
@@ -11,3 +12,39 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         GATTFanSwitch(coordinator),
         GATTHeatSwitch(coordinator),
     ])
+
+
+class GATTFanSwitch(SwitchEntity):
+    """Entity to control the fan."""
+
+    def __init__(self, coordinator):
+        self.coordinator = coordinator
+        self._attr_name = "Volcano Fan"
+
+    @property
+    def is_on(self):
+        return self.coordinator.data.get("Fan On")
+
+    async def async_turn_on(self):
+        await self.coordinator.client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
+
+    async def async_turn_off(self):
+        await self.coordinator.client.write_gatt_char("10110014-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
+
+
+class GATTHeatSwitch(SwitchEntity):
+    """Entity to control the heat."""
+
+    def __init__(self, coordinator):
+        self.coordinator = coordinator
+        self._attr_name = "Volcano Heat"
+
+    @property
+    def is_on(self):
+        return self.coordinator.data.get("Heat On")
+
+    async def async_turn_on(self):
+        await self.coordinator.client.write_gatt_char("1011000f-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
+
+    async def async_turn_off(self):
+        await self.coordinator.client.write_gatt_char("10110010-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
