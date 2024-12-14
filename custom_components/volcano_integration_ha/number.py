e3205226e3db53d5edd05a368ceb89987aa6e2c5
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-14 01:44:46 +0000

    Update number.py

--- custom_components/volcano_integration_ha/number.py
+++ custom_components/volcano_integration_ha/number.py
@@ -1,20 +1,12 @@
-from homeassistant.components.number import NumberEntity
+from . import GATTDeviceCoordinator
 
-class GATTSetTargetTemperature(NumberEntity):
-    """Entity to set the target temperature."""
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up Volcano Hybrid number entities."""
+    address = hass.data["volcano_integration_ha"][config_entry.entry_id]["address"]
 
-    def __init__(self, coordinator, device_info):
-        self.coordinator = coordinator
-        self._attr_name = "Set Target Temperature"
-        self._attr_device_info = device_info
-        self._attr_min_value = 40
-        self._attr_max_value = 230
-        self._attr_step = 1
+    coordinator = GATTDeviceCoordinator(hass, address, update_interval=0.5)
+    await coordinator.async_config_entry_first_refresh()
 
-    @property
-    def native_value(self):
-        return self.coordinator.data.get("temperature")
-
-    async def async_set_native_value(self, value):
-        async with BleakClient(self.coordinator.address) as client:
-            await client.write_gatt_char("10110003-5354-4f52-5a26-4249434b454c", int(value).to_bytes(2, "little"))
+    async_add_entities([
+        GATTSetTargetTemperature(coordinator),
+    ])
