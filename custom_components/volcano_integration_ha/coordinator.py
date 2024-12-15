9fabc6cd84991c1ebbd8c08425053012c05ed1bc
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-15 02:00:15 +0000

    Update coordinator.py

--- custom_components/volcano_integration_ha/coordinator.py
+++ custom_components/volcano_integration_ha/coordinator.py
@@ -1,27 +1,25 @@
-import logging
-from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-
-from .device import VolcanoDevice
+import logging
 
 _LOGGER = logging.getLogger(__name__)
+
 
 class VolcanoCoordinator(DataUpdateCoordinator):
     """Coordinator for polling the Volcano Hybrid."""
 
     def __init__(self, hass, address):
         """Initialize the coordinator."""
-        super().__init__(hass, _LOGGER, name="VolcanoCoordinator", update_interval=timedelta(seconds=0.5))
-        self.device = VolcanoDevice(address)
+        super().__init__(hass, _LOGGER, name="VolcanoCoordinator", update_interval=None)
+        self.device = None
 
     async def _async_update_data(self):
         """Fetch data from the Bluetooth device."""
+        if not self.device or not self.device.is_connected:
+            _LOGGER.warning("Device is not connected. Skipping update.")
+            return {}
+
         try:
             return await self.device.fetch_data()
         except Exception as e:
             _LOGGER.error(f"Error updating data: {e}")
             raise UpdateFailed(f"Error updating data: {e}")
-
-    async def shutdown(self):
-        """Shut down the Bluetooth connection."""
-        await self.device.disconnect()
