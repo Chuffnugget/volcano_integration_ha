51e5bfd04d044e02989dcf10edf40dfdc2018b57
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-15 01:47:39 +0000

    Update switch.py

--- custom_components/volcano_integration_ha/switch.py
+++ custom_components/volcano_integration_ha/switch.py
@@ -1,50 +1,30 @@
 from homeassistant.components.switch import SwitchEntity
-from .coordinator import GATTDeviceCoordinator
+from .device import VolcanoDevice
+
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up Volcano Hybrid switch entities."""
-    address = hass.data["volcano_integration_ha"][config_entry.entry_id]["address"]
-
-    coordinator = GATTDeviceCoordinator(hass, address, update_interval=0.5)
-    await coordinator.async_config_entry_first_refresh()
+    device = hass.data["volcano_integration_ha"][config_entry.entry_id]
 
     async_add_entities([
-        GATTFanSwitch(coordinator),
-        GATTHeatSwitch(coordinator),
+        GATTFanSwitch(device),
+        GATTHeatSwitch(device),
     ])
 
 
 class GATTFanSwitch(SwitchEntity):
     """Entity to control the fan."""
 
-    def __init__(self, coordinator):
-        self.coordinator = coordinator
+    def __init__(self, device):
+        self.device = device
         self._attr_name = "Volcano Fan"
 
     @property
     def is_on(self):
-        return self.coordinator.data.get("Fan On")
+        return self.device.data.get("fan_on")
 
     async def async_turn_on(self):
-        await self.coordinator.client.write_gatt_char("10110013-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
+        await self.device.write_characteristic("10110013-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
 
     async def async_turn_off(self):
-        await self.coordinator.client.write_gatt_char("10110014-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
-
-
-class GATTHeatSwitch(SwitchEntity):
-    """Entity to control the heat."""
-
-    def __init__(self, coordinator):
-        self.coordinator = coordinator
-        self._attr_name = "Volcano Heat"
-
-    @property
-    def is_on(self):
-        return self.coordinator.data.get("Heat On")
-
-    async def async_turn_on(self):
-        await self.coordinator.client.write_gatt_char("1011000f-5354-4f52-5a26-4249434b454c", bytearray([0x01]))
-
-    async def async_turn_off(self):
-        await self.coordinator.client.write_gatt_char("10110010-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
+        await self.device.write_characteristic("10110014-5354-4f52-5a26-4249434b454c", bytearray([0x00]))
