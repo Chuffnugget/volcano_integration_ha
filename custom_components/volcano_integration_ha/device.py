452fea8c1c495cd1d4f001fe9dad5f22b00381b4
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2024-12-15 01:59:48 +0000

    Update device.py

--- custom_components/volcano_integration_ha/device.py
+++ custom_components/volcano_integration_ha/device.py
@@ -7,7 +7,6 @@ UUIDS = {
     "fan_on": "10110013-5354-4f52-5a26-4249434b454c",
     "fan_off": "10110014-5354-4f52-5a26-4249434b454c",
     "temperature": "10110001-5354-4f52-5a26-4249434b454c",
-    # Add other UUIDs here...
 }
 
 
@@ -25,11 +24,11 @@ class VolcanoDevice:
         try:
             await self.client.connect()
             self.is_connected = True
-            self.status = None
+            self.status = "Connected"
             _LOGGER.info(f"Connected to {self.address}")
         except Exception as e:
-            self.status = f"Error connecting: {e}"
             self.is_connected = False
+            self.status = f"Error connecting: {e}"
             _LOGGER.error(self.status)
 
     async def disconnect(self):
@@ -37,7 +36,7 @@ class VolcanoDevice:
         try:
             await self.client.disconnect()
             self.is_connected = False
-            self.status = None
+            self.status = "Disconnected"
             _LOGGER.info(f"Disconnected from {self.address}")
         except Exception as e:
             self.status = f"Error disconnecting: {e}"
@@ -46,13 +45,13 @@ class VolcanoDevice:
     async def fetch_data(self):
         """Fetch data from the Bluetooth device."""
         if not self.is_connected:
-            raise Exception("Device is not connected")
+            _LOGGER.warning("Device is not connected")
+            return {}
+
         try:
             temperature_raw = await self.read_characteristic(UUIDS["temperature"])
             temperature = int.from_bytes(temperature_raw, byteorder="little") / 100.0
-            return {
-                "temperature": temperature,
-            }
+            return {"temperature": temperature}
         except Exception as e:
             _LOGGER.error(f"Error fetching data: {e}")
             raise
